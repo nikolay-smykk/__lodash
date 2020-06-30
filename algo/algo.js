8aeec93ae525596f0b4038bd61845a0d82c76d00
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2020-06-30 13:50:08 +0300

    areThereDuplicates args

--- algo/algo.js
+++ algo/algo.js
@@ -31,3 +31,17 @@ function areThereDuplicates() {
   }
   return false;
 }
+
+function areThereDuplicates(...args) {
+  args.sort((a, b) => a > b);
+  let start = 0;
+  let next = 1;
+  while (next < args.length) {
+    if (args[start] === args[next]) {
+      return true;
+    }
+    start++;
+    next++;
+  }
+  return false;
+}
