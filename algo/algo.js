e5e0308bde5a0ce0b4f596ecb6eab412654cc8db
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2020-07-08 13:50:58 +0300

    averagePair

--- algo/algo.js
+++ algo/algo.js
@@ -49,3 +49,15 @@ function areThereDuplicates(...args) {
 function areThereDuplicates() {
   return new Set(arguments).size !== arguments.length;
 }
+
+function averagePair(arr, num) {
+  let start = 0;
+  let end = arr.length - 1;
+  while (start < end) {
+    let avg = (arr[start] + arr[end]) / 2;
+    if (avg === num) return true;
+    else if (avg < num) start++;
+    else end--;
+  }
+  return false;
+}
