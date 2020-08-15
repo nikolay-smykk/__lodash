99ef450cdc8a7e63af24fbc4b1c3a578d4917cfa
Nikolay Smyk <41842687+nikolay-smykk@users.noreply.github.com>
2020-08-15 13:51:49 +0300

    maxSubarraySum

--- algo/algo.js
+++ algo/algo.js
@@ -79,3 +79,18 @@ function isSubsequence(str1, str2) {
   if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
   return isSubsequence(str1, str2.slice(1));
 }
+
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+
+  let total = 0;
+  for (let i = 0; i < num; i++) {
+    total += arr[i];
+  }
+  let currentTotal = total;
+  for (let i = num; i < arr.length; i++) {
+    currentTotal += arr[i] - arr[i - num];
+    total = Math.max(total, currentTotal);
+  }
+  return total;
+}
